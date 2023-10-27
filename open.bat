0c528b8f6083e1e445b6ca8fb07d18bed22758fb
Grand Admiral Thrawn <148997797+itsGrandAdmiralThrawn@users.noreply.github.com>
2023-10-27 22:19:01 +0530

    Create open.bat

---
+++ open.bat
@@ -0,0 +1,7 @@
+@echo off
+start https://linkedin.com/
+start https://facebook.com/
+start https://github.com/
+start https://hacktoberfest.com/profile/
+start https://www.youtube.com/
+start https://google.com/
