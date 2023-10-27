d3ee3b64bf709d4fc439378928f8a0185835e07a
Grand Admiral Thrawn <148997797+itsGrandAdmiralThrawn@users.noreply.github.com>
2023-10-27 22:17:41 +0530

    Create open.bat

---
+++ open.bat
@@ -0,0 +1,8 @@
+@echo off
+https://www.amazon.in/
+https://google.com/
+start https://github.com/
+start https://hacktoberfest.com/profile/
+start https://www.youtube.com/
+https://facebook.com/
+https://linkedin.com/
